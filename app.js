b27483dae5081e048b024e7c179dfae75e33cad5
Sridhar Voorakkara <sridhar.voorakkara@intel.com>
2016-03-02 21:53:57 +0000

    Added files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var routes = require('./modules/routes');
+
+app.use('/html', express.static('static'));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+app.get('/ws/rtpi/:stopid', routes.RTPIData);
